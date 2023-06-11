8d4aa0660eb2e6bf52e2d4604f3455511f185ed8
Alex Babilia <djesssash1708@gmail.com>
2023-06-11 20:32:02 +0300

    Added: User Registration

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 from fastapi import FastAPI
+
 from tortoise.contrib.fastapi import register_tortoise
-from db_models import (
-    User,
-    Business,
-    Product,
+from tortoise.signals import post_save
+from tortoise import BaseDBAsyncClient
+
+from typing import Optional, Type, List
+
+from db_models import User, Business, Product
+from pydentic_models import (
+    user_pydenticIn,
+    user_pydenticOut,
+    user_pydentic,
+    business_pydentic,
 )
- 
+from authentication import get_hashed_password
+
 app = FastAPI()
+
+@post_save(User)
+async def create_business(
+    sender: 'Type[User]', 
+    instance: User, 
+    created:bool, 
+    using_db: 'Optional[BaseDBAsyncClient]',
+    updated_fields: List[str]
+)->None :
+    if created:
+        business_obj = await Business.create(
+            name=instance.username, 
+            owner = instance,
+        )
+        
+        await business_pydentic.from_tortoise_orm(business_obj)
+        
+
+@app.post('/registration')
+async def user_registration(user: user_pydenticIn):
+    user_info = user.dict(exclude_unset=True)
+    user_info['password'] = get_hashed_password(user_info['password'])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydentic.from_tortoise_orm(user_obj)
+    return {
+        'status': 'OK',
+        'data': f'Hello, {new_user.username}, thanks for your registration,  check your email'
+    }
+
+
 
 @app.get('/')
 def index():
--- pydentic_models.py
+++ pydentic_models.py
@@ -1,13 +1,13 @@
-from tortoise.contrib.pydantic import pydentic_model_creator
+from tortoise.contrib.pydantic import pydantic_model_creator
 
-from .db_models import User, Business, Product
+from db_models import User, Business, Product
 
-user_pydentic = pydentic_model_creator(User, name='User', exclude=('is_virified', ))
-user_pydenticIn = pydentic_model_creator(User, name='UserIn', exclude_readonly=True)
-user_pydenticOut = pydentic_model_creator(User, name='UserOut', exclude=('password', ))
+user_pydentic = pydantic_model_creator(User, name='User', exclude=('is_virified', 'join_date'))
+user_pydenticIn = pydantic_model_creator(User, name='UserIn', exclude_readonly=True)
+user_pydenticOut = pydantic_model_creator(User, name='UserOut', exclude=('password', ))
 
-business_pydentic = pydentic_model_creator(Business, name='Business')
-business_pydenticIn = pydentic_model_creator(Business, name='BusinessIn', exclude_readonly=True)
+business_pydentic = pydantic_model_creator(Business, name='Business')
+business_pydenticIn = pydantic_model_creator(Business, name='BusinessIn', exclude_readonly=True)
 
-product_pydentic = pydentic_model_creator(Product, name='Product')
-product_pydenticIn = pydentic_model_creator(Product, name='ProductIn', exclude=('percentage_discount', 'id', ))
+product_pydentic = pydantic_model_creator(Product, name='Product')
+product_pydenticIn = pydantic_model_creator(Product, name='ProductIn', exclude=('percentage_discount', 'id', ))

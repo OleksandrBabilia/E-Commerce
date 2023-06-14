de124b9faec89472bde7c55a02cec63357472594
Alex Babilia <djesssash1708@gmail.com>
2023-06-14 12:42:37 +0300

    Added: Prodcut Image Upload

--- main.py
+++ main.py
@@ -157,7 +157,55 @@ async def create_upload_file(file: UploadFile=File(...), user: user_pydentic=Dep
         'status': 'OK',
         'filename': file_url
     }
+
+@app.post('/uploadfile/product/{id}')
+async def create_upload_file(id: int, file: UploadFile=File(...), 
+                                user: user_pydentic=Depends(get_current_user)): 
+    FILEPATH = './static/images/'
+    filename = file.filename
+    extension = filename.split('.')[1]
+    
+    if extension not in ['jpg', 'png']:
+        return {
+            'status': 'ERROR',
+            'detail': 'File extension not allowed'
+        }
         
+    token_name = f'{secrets.token_hex(10)}.{extension}'
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    
+    with open(generated_name, 'wb') as file:
+        file.write(file_content)
+        
+    img = Image.open(generated_name)
+    img = img.resize(size=(200, 200))
+    img.save(generated_name)
+    
+    file.close()
+    
+    product = await Product.get(id=id)
+    business = await Product.business
+    owner = await business.owner
+    
+    if owner == user:
+        product.product_image = token_name
+        await product.save()
+        
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Not Authenticated to perform this action',
+            headers={'WWW-Authenticate': 'Bearer'}
+        )
+    
+    file_url = 'localhost:8000' + generated_name[1:]
+    
+    return {
+        'status': 'OK',
+        'filename': file_url
+    }
+    
 
 register_tortoise(
     app,
